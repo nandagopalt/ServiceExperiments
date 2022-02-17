628e4c9a3c8892541804acefebd329fd4d880f1d
Nalan Sarrwin RT <gopikrish.thiru@gmail.com>
2022-02-17 18:15:32 +0530

    Added few comments for better understanding of the code

--- ServiceSampleExample/app/src/main/java/com/amalwin/servicesampleexample/MyBackgroundService.kt
+++ ServiceSampleExample/app/src/main/java/com/amalwin/servicesampleexample/MyBackgroundService.kt
@@ -7,6 +7,7 @@ import android.util.Log
 
 class MyBackgroundService : Service() {
     override fun onBind(intent: Intent): IBinder? = null
+    // Code block to be called when the class MyBackgroundService gets instantiated from the client
     init {
         Log.i(TAG, "Service instantiated / created in the memory !")
     }
@@ -15,8 +16,9 @@ class MyBackgroundService : Service() {
         Log.i(TAG, "Service running !")
         val name = intent.getStringExtra(NAME)
         val marks = intent.getIntExtra(MARKS, 78)
-        Log.i(TAG, "Name : $name and Marks : $marks")
-        return START_STICKY
+        Log.i(TAG, "Name : $name, Marks : $marks , startId: $startId")
+        return START_STICKY // Android system may restart the service at the time of memory crunch or during the prioritization of the application execution and while
+                            // restarting the flag indicates the android system whether to pass the intent. "START_STICKY" allows service to restart but not to pass the intent that was lastly executed.
     }
 
     override fun onDestroy() {
